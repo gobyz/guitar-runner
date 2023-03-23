cee25151b74a8bd51c1a3e78e1b792b1a71c637c
gobyz <gobyz994@gmail.com>
2023-03-23 19:36:09 +0100

    add mainmenu scene / add main menu layout / add tutorial layout

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneName;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
